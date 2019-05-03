ddfd9130ea01f78f4080c864f15e3b997856d0c5
hauke <hauke.dau@opitz-consulting.com>
2019-05-04 00:57:43 +0200

    first experiment with linear regression and not normalized spectrogram

--- mlpModel.py
+++ mlpModel.py
@@ -7,7 +7,12 @@ import numpy as np
 class LinearRegressionModel(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(LinearRegressionModel,self).__init__()
-        self.linear = nn.Linear(input_dim, output_dim)
+        print("input_dim", input_dim)
+        print("output_dim", output_dim)
+        self.linear = nn.Sequential(
+            nn.Linear(input_dim, output_dim*3),
+            nn.ReLU(),
+            nn.Linear(output_dim*3, output_dim))
 
     def forward(self, x):
         out = self.linear(x)
@@ -15,32 +20,36 @@ class LinearRegressionModel(nn.Module):
 
 
 def training(input, wanted):
-    model = LinearRegressionModel(input.size, wanted.size)
+    model = LinearRegressionModel(input.shape[0], wanted.shape[0]).cuda()
     criterion = nn.MSELoss()
 
-    learning_rate = 0.01
+    learning_rate = 0.00005
     optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate)
 
-    epochs = 100
+    epochs = 1000
 
     for epoch in range(epochs):
         epoch += 1
-        input_var = Variable(torch.from_numpy(input))
-        wanted_var = Variable(torch.from_numpy(wanted))
-        optimizer.zero_grad()
-        output_model = model(input_var)
-        loss = criterion(output_model,wanted_var)
-        loss.backward()
-        optimizer.step()
+        for step in range(input.shape[0]):
+            step += 1
+            value_in = input[:, step]
+            value_res = wanted[:, step]
+
+            np_ft32_in = np.array(value_in, dtype=np.float32)
+            np_ft32_res = np.array(value_res, dtype=np.float32)
+
+            input_var = Variable(torch.from_numpy(np_ft32_in)).cuda()
+            wanted_var = Variable(torch.from_numpy(np_ft32_res)).cuda()
+            optimizer.zero_grad()
+
+            output_model = model(input_var)
+            loss = criterion(output_model,wanted_var)
+            loss.backward()
+            optimizer.step()
+            print('step {}, loss {}'.format(step, loss.data))
         print('epoch {}, loss {}'.format(epoch, loss.data))
+
+
     return model
 
-
-input = np.array([0,1,2,3,4,5],dtype= np.float32)
-wanted = np.array([3,4,5,6,7,8],dtype= np.float32)
-
-model = training(input,wanted)
-
-predicted = model(Variable(torch.from_numpy(input))).data.numpy()
-print(predicted)
 
--- sequenzial_processing.py
+++ sequenzial_processing.py
@@ -1,22 +1,25 @@
 import librosa
 import numpy as np
-import mlpModel
+import mlpModel as mlp
+import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
-wav, sr = librosa.load("./inWav/bbad4n.wav")
+wav, sr = librosa.load("./inWav/arctic_indian_man16.wav")
 stft_in = np.abs(librosa.stft(wav))**2
 mel_in = librosa.feature.melspectrogram(S=stft_in)
 stft_in = np.array(stft_in)
 mel_in = np.array(mel_in)
-print("wav:", wav)
 
 print("-----------------------------------")
 print("stft_in Size:", stft_in.shape)
-print("stft_in:", stft_in)
+
 
 print("-----------------------------------")
-print("mel_in Size:", mel_in.shape)
-print("mel_in:", mel_in)
+print("mel_in Shape:", mel_in.shape)
+print("mel_in Size:", mel_in.shape[1])
+model = mlp.training(mel_in, stft_in)
 
-
+#predicted = model(Variable(torch.from_numpy(mel_in[:,0]))).data.numpy()
+#print(predicted)
 
