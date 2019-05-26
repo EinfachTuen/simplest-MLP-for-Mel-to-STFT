022c1179f49bcf80aa47abb8b0382f781aa6204e
haukedau <hauke.dau@googlemail.com>
2019-05-27 01:09:05 +0200

    experiment with correct transformed stft

--- makeAudioFromMel.py
+++ makeAudioFromMel.py
@@ -22,9 +22,18 @@ def plotSTFT(stft,title):
     plt.title(title)
     plt.show()
 
+def mel_to_stft(mel,sr):
+    mel_basis = librosa.filters.mel(sr, 2048, n_mels=mel.shape[0],
+                            dtype=mel.dtype)
+
+    stft_again = np.dot(np.swapaxes(mel_basis,0,1),mel)
+    # Find the non-negative least squares solution, and apply
+    # the inverse exponent.
+    # We'll do the exponentiation in-place.
+    return np.power(stft_again, 1./2.00, out=stft_again)
 
 mel,sr = generateMel("./reserveWav/arctic_indian_man16.wav")
-stft_out = librosa.feature.inverse.mel_to_stft(mel, sr=sr)
+stft_out = mel_to_stft(mel, sr=sr)
 
 wav = librosa.istft(stft_out)
 plotSTFT(stft_out,'stft_out')
